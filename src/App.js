7e006bbc76a0a71bee4bd11c0a4f6e25d1da351e
unknown <iarafat386@gmail.com>
2021-08-01 23:17:17 +0600

    HTML body created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState("hey");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <form action="">
+          <input type="text" name="" id="" value={result} />
+        </form>
+        <div className="keypad">
+          <button>clear</button>
+          <button>c</button>
+          <button>&divide;</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>&times;</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>&ndash</button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+    </>
   );
 }
 

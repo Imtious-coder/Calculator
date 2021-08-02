d604caf03494161fb6fd7abb8c429be9db997637
unknown <iarafat386@gmail.com>
2021-08-02 11:48:13 +0600

    Readme Updated

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // State...
   const [result, setResult] = useState("");
 
+  // Functions..
   const handleClick = (e) => {
     setResult(result.concat(e.target.name));
   }
@@ -28,26 +30,40 @@ function App() {
     <>
       <div className="container">
         <form action="">
+          {/* Display */}
           <input type="text" name="" id="" value={result} placeholder="Welcome!"/>
         </form>
+        {/* ...Keypad... */}
         <div className="keypad">
+          {/* All Clear button */}
           <button className="highlight" onClick={clear} id="clear">Clear</button>
+          {/* Clear button */}
           <button className="highlight" onClick={backspace} id="backspace">c</button>
+          {/* Divide button */}
           <button className="highlight" name="/" onClick={handleClick}>&divide;</button>
+          {/* Numbers */}
           <button name="7" onClick={handleClick}>7</button>
           <button name="8" onClick={handleClick}>8</button>
           <button name="9" onClick={handleClick}>9</button>
+          {/* Multiply button */}
           <button className="highlight" name="*" onClick={handleClick}>&times;</button>
+          {/* Numbers */}
           <button name="4" onClick={handleClick}>4</button>
           <button name="5" onClick={handleClick}>5</button>
           <button name="6" onClick={handleClick}>6</button>
+          {/* Minus button */}
           <button className="highlight" name="-" onClick={handleClick}>&ndash;</button>
+          {/* Numbers */}
           <button name="1" onClick={handleClick}>1</button>
           <button name="2" onClick={handleClick}>2</button>
           <button name="3" onClick={handleClick}>3</button>
+          {/* Sum button */}
           <button className="highlight" name="+" onClick={handleClick}>+</button>
+          {/* Number */}
           <button name="0" onClick={handleClick}>0</button>
+          {/* Dot button */}
           <button className="highlight" name="." onClick={handleClick}>.</button>
+          {/* Final Result Button */}
           <button className="highlight" onClick={calculate} id="result">=</button>
         </div>
       </div>
